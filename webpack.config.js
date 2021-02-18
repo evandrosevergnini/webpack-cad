bc5ea82e20e17ca02d9f8611f02870b2131b534e
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-02-18 16:43:48 -0300

    Configurando HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,24 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      filename: "indice.html",
       template: path.resolve(__dirname, "src", "index.html"),
     }),
-    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({  // Also generate a page.html
+      filename:  "page.html",
+      template: path.resolve(__dirname, "src", "page.html"),
+      title: "Page"
+    }),
+    new HtmlWebpackPlugin({  // Also generate a page.html
+      filename:  "other-page.html",
+      template: path.resolve(__dirname, "src", "other-page.html"),
+      title: "Other Page"
+    }),
   ],
   devServer: {
+    openPage: 'indice.html',
     port: 8091,
     overlay: true,
     open: "Google Chrome", // 'Google Chrome' on macOS, 'google-chrome' on Linux and 'chrome' on Windows
