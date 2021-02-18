cdb7dacc62c98b9da692cebdc1713777c138f94c
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-02-17 23:15:02 -0300

    Removendo arquivos da pasta dist para gerar um novo build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: { 
-    main: path.resolve(__dirname, "src", "main.js") 
+  entry: {
+    main: path.resolve(__dirname, "src", "main.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name]-[chunkhash].js",
-    publicPath: "/"
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -27,5 +28,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new CleanWebpackPlugin(),
   ],
 };
