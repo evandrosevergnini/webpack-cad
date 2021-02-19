7d061de13d2db94d39bd5868f7cdb8eca4343c38
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-02-19 17:06:09 -0300

    Carregando assets

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./assets/scss/main.scss";
+//import logo from '/src/assets/images/logo.png';
 const modernFunc = () => {
   console.log("Hello webpack with Babel!");
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 module.exports = {
   entry: {
     main: path.resolve(__dirname, "src", "main.js"),
@@ -12,6 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name]-[chunkhash].js",
     publicPath: "/",
+    assetModuleFilename: "assets/[name]-[hash:8][ext]",
   },
   module: {
     rules: [
@@ -24,6 +24,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
@@ -31,10 +39,10 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new FaviconsWebpackPlugin({
       logo: path.resolve(__dirname, "src/assets/images/logo.png"), // svg works too!
-      mode: 'webapp', // optional can be 'webapp', 'light' or 'auto' - 'auto' by default
-      devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default 
-      prefix: 'assets/pwa/', // Prefix path for generated assets
-      manifest: './src/manifest.json',
+      mode: "light", // optional can be 'webapp', 'light' or 'auto' - 'auto' by default
+      devMode: "light", // optional can be 'webapp' or 'light' - 'light' by default
+      prefix: "assets-pwa/", // Prefix path for generated assets
+      manifest: "./src/manifest.json",
       // favicons: {
       //   appName: 'W CAD',
       //   appDescription: 'W Cad App',
@@ -52,21 +60,23 @@ module.exports = {
       filename: "indice.html",
       template: path.resolve(__dirname, "src", "index.html"),
     }),
-    new HtmlWebpackPlugin({  // Also generate a page.html
-      filename:  "page.html",
+    new HtmlWebpackPlugin({
+      // Also generate a page.html
+      filename: "page.html",
       template: path.resolve(__dirname, "src", "page.html"),
-      title: "Page"
+      title: "Page",
     }),
-    new HtmlWebpackPlugin({  // Also generate a page.html
-      filename:  "other-page.html",
+    new HtmlWebpackPlugin({
+      // Also generate a page.html
+      filename: "other-page.html",
       template: path.resolve(__dirname, "src", "other-page.html"),
-      title: "Other Page"
+      title: "Other Page",
     }),
   ],
   devServer: {
-    openPage: 'indice.html',
+    openPage: "indice.html",
     port: 8091,
     overlay: true,
     open: "Google Chrome", // 'Google Chrome' on macOS, 'google-chrome' on Linux and 'chrome' on Windows
-  }
+  },
 };
